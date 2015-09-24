9c5235a838fc4360984e14416ee59797782f5963
meihao <meihaohpoto@gmail.com>
2015-09-24 22:33:35 +0900

    2015.09.24　V0.1環境テスト済み

---
+++ src/hello/MyServlet.java
@@ -0,0 +1,60 @@
+package hello;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public MyServlet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=Shift_JIS");
+		PrintWriter out = response.getWriter();
+
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>HelloServlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+
+		out.println("<p>テスト環境</p>");
+		out.println("<p>環境確認ページ</p>");
+
+		out.println("</body>");
+		out.println("</html>");
+
+		out.close();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
